58bf4b87767d40e77b079179b4dd499c50bd4711
Marvish Subhas Chandra <mschandr@uci.edu>
2022-08-20 14:32:35 -0700

    Add files via upload

---
+++ trackingweatherdisasters.java
@@ -0,0 +1,23 @@
+
+class weatherDisasters:
+
+public static void westCoast(String[] weatherDisaster){
+    if weatherDisaster = "earthquake"
+    if weatherDiaster = "heat waves":
+    System.out.PrintIn()
+}
+
+
+public static void eastCoast(String[] weatherDisaster){
+    if weatherDisaster = "hurricane"
+    System.out.PrintIn()
+    if weatherDiaster = "tsunami"
+    System.out.PrintIn()
+    if weatherDisaster = "flooding"
+    System.out.PrintIn()
+}
+
+public static void centralCoast(String[] weatherDisaster){
+    if weatherDisaster = "lack of agriculture"
+    System.out.PrintIn()
+}
