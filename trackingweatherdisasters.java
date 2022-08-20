3edd406269d3b6ac84cda86ffaf4884525563be0
Marvish Subhas Chandra <mschandr@uci.edu>
2022-08-20 14:53:10 -0700

    Add files via upload

--- trackingweatherdisasters.java
+++ trackingweatherdisasters.java
@@ -2,22 +2,37 @@
 class weatherDisasters:
 
 public static void westCoast(String[] weatherDisaster){
-    if weatherDisaster = "earthquake"
+    if weatherDisaster = "earthquake":
+    System.out.PrintIn("Make sure to find a nearby desk or table and cower underneath. Do not attempt to walk or run outside.")
     if weatherDiaster = "heat waves":
-    System.out.PrintIn()
+    System.out.PrintIn("Because of esclated temperatures, stay hydrated by drinking enough water and staying inside if possible.")
+    if weatherDiaster = "flooding":
+    System.out.PrintIn("Make sure to be inside when this occurs, have any important belongings inside. If flooding state is terrible, grab sand bags and cover your home's surroundings with them.")
+    if weatherDisaster = "hail storms":
+    System.out.PrintIn("During this scenario, make sure to find shelter immediately. Based on severity, the size of hail debris can cause damage to home and automobile's roofs.")
+    else: System.out.PrintIn("Make sure to keep up to date about your weather and stay indoors if any potential weather disasters may occur.")
 }
 
 
 public static void eastCoast(String[] weatherDisaster){
-    if weatherDisaster = "hurricane"
-    System.out.PrintIn()
-    if weatherDiaster = "tsunami"
-    System.out.PrintIn()
-    if weatherDisaster = "flooding"
-    System.out.PrintIn()
+    if weatherDisaster = "hurricane":
+    System.out.PrintIn("Find shelter immediately and find the nearest underground basement to defend yourself in.")
+    if weatherDiaster = "tsunami":
+    System.out.PrintIn("Run to higher ground and do not attempt to go towards the tsunami at all. The tsunami creates dangerous storm conditions that may harm you or your loved ones.")
+    if weatherDisaster = "flooding":
+    System.out.PrintIn("Make sure to be inside when this occurs, have any important belongings inside. If flooding state is terrible, grab sand bags and cover your home's surroundings with them.")
+    if weatherDisaster = "excessive snowing":
+    System.out.PrintIn("Make sure to be inside, wear enough layers, and shovel the surroundings of your home.")
+    else: System.out.PrintIn("Make sure to keep up to date about your weather and stay indoors if any potential weather disasters may occur.")
 }
 
 public static void centralCoast(String[] weatherDisaster){
-    if weatherDisaster = "lack of agriculture"
-    System.out.PrintIn()
+    if weatherDiaster = "tornado":
+    System.out.PrintIn("Run or locate yourself away from the tornado, so any of the wind does not get close to you.")
+    if weatherDisaster = "flooding":
+    System.out.PrintIn("Make sure to be inside when this occurs, have any important belongings inside. If flooding state is terrible, grab sand bags and cover your home's surroundings with them.")
+    if weatherDisaster = "hail storms":
+    System.out.PrintIn("During this scenario, make sure to find shelter immediately. Based on severity, the size of hail debris can cause damage to home and automobile's roofs.")
+   else: System.out.PrintIn("Make sure to keep up to date about your weather and stay indoors if any potential weather disasters may occur.")
+    
 }
